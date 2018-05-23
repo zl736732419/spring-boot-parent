d5abafd5eb2268b9e09d2a29b754fef40596717e
z736732419 <736732419@qq.com>
2018-05-24 00:13:36 +0800

    添加按照用户名查找session

--- spring-boot-session-redis/src/main/java/com/zheng/springboot/controller/SessionController.java
+++ spring-boot-session-redis/src/main/java/com/zheng/springboot/controller/SessionController.java
@@ -1,5 +1,8 @@
 package com.zheng.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -16,6 +20,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/session")
 public class SessionController {
+
+    @Autowired
+    FindByIndexNameSessionRepository<? extends Session> sessionRepository;
+
     @GetMapping("/hello/{str}")
     public String hello(@PathVariable String str, HttpServletRequest request) {
         String key = "springboot";
@@ -24,7 +32,21 @@ public class SessionController {
         if (!Optional.ofNullable(o).isPresent()) {
             o = "spring boot session test";
             session.setAttribute(key, o);
+        } else {
+            System.out.println(o);
         }
+
+        // 通过username查找到session
+        String username = "zhangsan";
+        session.setAttribute(
+                FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, username);
+        
         return "sessionId:" + session.getId();
+    }
+
+    @GetMapping("/findByUsername/{username}")
+    public Map findByUsername(@PathVariable String username) {
+        Map<String, ? extends Session> usersSessions = sessionRepository.findByIndexNameAndIndexValue(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, username);
+        return usersSessions;
     }
 }
