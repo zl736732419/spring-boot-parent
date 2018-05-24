54fc884a22073a91338874db1355d8b2e59fc5f5
zhenglian <736732419@qq.com>
2018-05-24 17:36:32 +0800

    添加数据库脚本通过liquibase初始化

---
+++ spring-boot-dbinit-liquibase/src/main/java/com/zheng/springboot/config/JdbcConfig.java
@@ -0,0 +1,22 @@
+package com.zheng.springboot.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * @Author zhenglian
+ * @Date 2018/5/23 22:40
+ */
+@Configuration
+public class JdbcConfig {
+
+    @Primary
+    @Bean(destroyMethod = "close", initMethod = "init")
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DruidDataSource dataSource() {
+        return new DruidDataSource();
+    }
+}
